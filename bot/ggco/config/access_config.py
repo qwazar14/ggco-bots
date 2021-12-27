9172ead64161f2db7286ebdeedd2fdeacc86117d
qwazar14 <zenez89@gmail.com>
2021-12-28 01:10:06 +0200

    добавлены конфиги

---
+++ bot/ggco/config/access_config.py
@@ -0,0 +1,7 @@
+settings = {
+    'botToken': 'OTI0MDUyMTUzNzUwOTQxNzE2.YcY8oQ.sgtieA_XM8xvjSkAHNYXPmlcVB0',
+    'botName': 'GGCO Bot',
+    'botId': 899687265654149151,
+    'botPrefix': 'a!',
+    'guildId': 398857722159824907
+}
