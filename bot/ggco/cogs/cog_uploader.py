12b4384992279c13078f08960f524b765bfbeb14
qwazar14 <zenez89@gmail.com>
2022-01-15 17:56:27 +0200

    cog uploader throw DS

---
+++ bot/ggco/cogs/cog_uploader.py
@@ -0,0 +1,24 @@
+from disnake.ext import commands
+
+from config import roles_config
+
+
+class CogUploader(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # @commands.slash_command(name="upload_cog", description='Загружает когу в папку с когами')
+    @commands.has_any_role(roles_config.discord_roles['admin'])
+    @commands.command()
+    async def upload_cog(self, ctx):
+        try:
+            for attach in ctx.message.attachments:
+                await attach.save(f"bot/ggco/cogs/{attach.filename}")
+                await ctx.message.add_reaction('✅')
+        except Exception as e:
+            await ctx.message.add_reaction('❌')
+
+
+def setup(bot):
+    bot.add_cog(CogUploader(bot))
